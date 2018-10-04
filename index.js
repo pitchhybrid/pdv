5d65e7071b232fb659653edd617b9497a4b4614f
pitchhybrid <30882490+pitchhybrid@users.noreply.github.com>
2018-10-03 21:45:44 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+let produtos = [];
+
+
 function insereLista() {
-    const carrinho = document.getElementById("sacola");
-    let qtd = document.getElementById("qtd");
-    let prod = document.getElementById("prod");
-    let itemLista = document.createElement("li");
+    let qtd = $("#qtd");
+    let prod = $("#prod");
+    produtos.push({
+        produtoNome : prod.val(),
+        quantidade : qtd.val(),
+        preco : Math.round((Math.random()*100)+1)
+    });
 
-    itemLista.textContent = `${prod.value} Quantidade:   ${qtd.value}`;
-    carrinho.appendChild(itemLista);
-
-
-
+    atualizarCarrinho();
 }
+
+function deleteById(id){
+   
+    produtos = produtos.filter((e,i)=>{
+        return i != id;
+    })
+console.log(produtos)
+atualizarCarrinho();
+}
+
+function atualizarCarrinho(){
+    const carrinho = $("#sacola");
+    const total = $("#total");
+
+    let html = '';
+    let valorTotal = 0;
+    for (let index = 0; index < produtos.length; index++) {
+        const produto = produtos[index];
+        valorTotal += produto.preco*produto.quantidade;
+        html += `<tr>
+            <td>${produto.produtoNome}</td> 
+            <td>${produto.quantidade}</td> 
+            <td>${produto.preco}</td>
+            <td><button class="btn btn-primary" onclick="deleteById(${index})"><i class="far fa-trash-alt"></i></button></td>
+            </tr>`
+    }
+    total.html("R$"+valorTotal)
+    carrinho.html(html);
+}
+
+func
+
