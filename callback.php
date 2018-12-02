9853627107486538ceee5b21d23ccd8d605d70e4
junior <pitchhybrid@junior>
2018-12-02 02:04:37 -0300

    pdv

--- callback.php
+++ callback.php
@@ -1,14 +1,49 @@
 <?php
-$loginMock ="admin";
-$passMock = "admin";
-verificar_login($_POST['user'],$_POST['pass']);
+include('./handler.php');
+if( isset($_POST['user']) && isset($_POST['pass']) ){
+  $login = $_POST['user'];
+  $pass =  $_POST['pass'];
 
-function verificar_login($l,$p){
-    global $loginMock,$passMock;
-    if($l == $loginMock && $p == $passMock){
-       return header('location: ./dashboard.php');
-        }else{
-       return header('location: ./index.php');
-            
-        }
+  $data = $dbh->prepare("SELECT * FROM usuarios WHERE nome='$login' AND senha='$pass'");
+  $data->execute();
+  $row = $data->fetch();
+
+  if ($row) {
+    return header('location: ./dashboard.php');
+  }else {
+    return header('location: ./index.php?erro=1');
+  }
+
 }
+
+if( isset($_POST['caduser']) && isset($_POST['cadpass']) && isset($_POST['cadpass'])){
+$cadlogin = $_POST['caduser'];
+$cadpass =  $_POST['cadpass'];
+$cadpass1 =  $_POST['cadpass1'];
+
+$data = $dbh->prepare("SELECT * FROM usuarios WHERE nome='$cadlogin'");
+$data->execute();
+$row = $data->fetch();
+
+if ($row) {
+  return header('location: ./index.php?erro=2');
+}else {
+
+    if($cadpass == $cadpass1){
+
+      $data = $dbh->prepare("INSERT INTO usuarios(nome,senha) VALUES (?,?)");
+      $data->bindParam(1,$cadlogin,PDO::PARAM_STR);
+      $data->bindParam(2,$cadpass,PDO::PARAM_STR);
+      $data->execute();
+      $data = NULL;
+      return header('location: ./dashboard.php');
+    }else {
+      return header('location: ./index.php?erro=3');
+    }
+
+}
+
+
+
+
+ }
--- index.php
+++ index.php
@@ -15,18 +15,72 @@ cabecalho("Login");
                 <div class="form-group display-4" style="font-size:1.5rem;">
                     <label class="my-4">Login:</label>
                     <input required class="form-control" type="text" id="user" name="user">
-            
+
                     <label class="my-4">Senha:</label>
                     <input required class="form-control" type="password" id="senha" name="pass">
-                    
-                    <div class="my-3 float-right">
-                    <button class="btn btn-primary" type="submit"><i class="fas fa-sign-in-alt"></i></button>
+
+                    <div class="my-3 ">
+                    <button class="btn btn-primary float-right" type="submit"><i class="fas fa-sign-in-alt"></i></button>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary float-left" data-toggle="modal" data-target="#exampleModal">
+                      Cadastrar
+                    </button>
                     </div>
                 </div>
-        </div>
-        </form>
+            </form>
+            <p class="text-center display-4" style="font-size:1.1rem; color:red;">
+              <?php
+            if (isset($_GET['erro']))
+            {
+              if ($_GET['erro'] == 1) {
+                echo "<b>Usuario ou Senha incorretos </b>";
+              }
+              if ($_GET['erro'] == 2) {
+                echo "<b>Usuario ja cadastrado </b>";
+              }
+              if ($_GET['erro'] == 3) {
+                echo "<b>senhas não conferem</b>";
+              }
 
+            }
+             ?>
+           </p>
+        </div>
     </div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body display-4">
+
+          <form action="./callback.php" method="post" style="font-size:1.5rem;">
+
+            <label class="my-4">Login:</label>
+            <input required class="form-control" type="text" name="caduser">
+
+            <label class="my-4">Senha:</label>
+            <input required class="form-control" type="password" name="cadpass">
+
+            <label class="my-4">Confirmar senha:</label>
+            <input required class="form-control" type="password" name="cadpass1">
+
+            <button class="my-4 btn btn-primary float-right" type="submit">Cadastrar</button>
+          </form>
+
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 <footer>
 <?php
